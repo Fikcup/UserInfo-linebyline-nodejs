902971992cc483e2b7488e199ceb5243891711e6
Fikcup <rhys.n.wright@gmail.com>
2021-06-02 09:43:40 -0700

    docs : added next steps

--- index.js
+++ index.js
@@ -18,16 +18,19 @@ function getUserInput()
     rl.question("What is your name? \n", (name) => {
         console.log(name);
         rlname = name;
+        // Log answer in JSON
     })
 
     rl.question("What is your age? \n", (age) => {
         console.log(age);
         rlage = age;
+        // Log answer in JSON
     })
 
     rl.question("What is your favorite color? \n", (color) => {
         console.log(color);
         rlcolor = color;
+        // Log answer in JSON
     })
 
     console.log("Hello " + rlname + ", you are " + rlage + " years old and your favorite color is " + rlcolor + ".");
