addf1565d3bfbe8c91e6ab67d380d979ea3cadd8
Fikcup <rhys.n.wright@gmail.com>
2021-06-02 09:23:08 -0700

    feat : created a readline and declared variables

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+// Create variables to store local variables grabbed
+var rlname;
+var rlage;
+var rlcolor;
+
 // Create reading interface
-const readLine = require('readline').createInterface ({
+const readline = require('readline');
+
+const rl = readline.createInterface ({
     input: process.stdin,
     output: process.stdout,
     prompt: '>'
@@ -8,20 +15,22 @@ const readLine = require('readline').createInterface ({
 // Ask a question
 function getUserInput()
 {
-    readLine.question("What is your name?", (name) => {
+    rl.question("What is your name?", (name) => {
         console.log(name);
+        rlname = name;
     })
 
-    readLine.question("What is your age?", (age) => {
+    rl.question("What is your age?", (age) => {
         console.log(age);
+        rlage = age;
     })
 
-    readLine.question("What is your favorite color?", (color) => {
+    rl.question("What is your favorite color?", (color) => {
         console.log(color);
+        rlcolor = color;
     })
 
-    console.log("Hello " + name + ", you are " + age + " years old and your favorite color is " + color + ".");
+    console.log("Hello " + rlname + ", you are " + rlage + " years old and your favorite color is " + rlcolor + ".");
 }
 
 getUserInput();
-readLine.stop();
